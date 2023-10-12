95e3fe24ca120ec4ce95081da134427e6f8a3e2b
MohamedMankouchi <mohamed.mankouchi@student.ehb.be>
2023-10-12 11:26:15 +0200

    Get all users from database

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
+app.get("/users", async (req, res) => {
+  const users = await knex("users").select();
+  res.json(users);
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
