48a9483d5ed30403ead3feab0b2ee2d9bc4f90d0
MohamedMankouchi <mohamed.mankouchi@student.ehb.be>
2023-12-03 19:40:50 +0100

    Added a protectedRoute that can only be accessed if connected with username

--- images/app/app/src/App.tsx
+++ images/app/app/src/App.tsx
@@ -1,15 +1,30 @@
 import { useState } from "react";
-
+import { useLocation } from "react-router-dom";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState<string>("");
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (input == "") {
+      return alert("Please fill in a username");
+    }
+    localStorage.setItem("username", input);
+    setInput("");
+    window.location.href = "/users";
+  };
   return (
     <>
       <div className="home">
         <div className="home-title">
           <p>Enter your username to get access</p>
-          <form className="home-title-inputs">
-            <input type="text" placeholder="Enter a username" />
+          <form className="home-title-inputs" onSubmit={handleSubmit}>
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Enter a username"
+            />
             <button>OK</button>
           </form>
         </div>
--- images/app/app/src/main.tsx
+++ images/app/app/src/main.tsx
@@ -1,9 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ProtectedRoute } from "./Routes/ProtectedRoute.tsx";
+import { Users } from "./Routes/Users.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+
+  {
+    path: "/users",
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "",
+        element: <Users />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
